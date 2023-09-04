bf9f66d158a7795f5514b723e8e0498c25de8d3b
Your Name <you@example.com>
2023-09-05 00:50:31 +0530

    js interview

--- jscourse/index.js
+++ jscourse/index.js
@@ -1,8 +1,38 @@
 // var dulha ="lab"
 // var dulhan ="laby"
 
-const dulha ="lab"
-const  dulhan ="laby"
+// const dulha ="lab"
+// const  dulhan ="laby"
+
+// var a = [1,2,23,34,4,23 ,4,4 ,45,54, 3,23];
+
+// a.forEach(function(val){
+//       console.log (val+2);
+// })
 
 
 
+// var obj ={
+//     name :"studyhard",
+//     age :"25",
+//     city : "moneland",
+// }
+
+// for(var key in obj) {
+//     console.log(key, obj[key]);
+// }
+
+
+// setTimeout(function(){
+   
+//     console.log("workhard")
+
+// },2000);
+
+
+
+function abcd (a) {
+    a();
+}
+
+abcd (function(){console.log("xyz")})
