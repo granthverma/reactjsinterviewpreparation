e86ee94ca04a494e7da67473154e23ad244cb363
Your Name <you@example.com>
2023-09-09 17:40:14 +0530

    Sidcup Family Golf websit

--- projecthtmlcss/script.js
+++ projecthtmlcss/script.js
@@ -1,6 +1,9 @@
 var crsr = document.querySelector("#cursor");
 var blur = document.querySelector("#cursor-blur");
 
+// html doucoment use for js 
+
+
 document.addEventListener("mousemove", function (dets) {
   crsr.style.left = dets.x + "px";
   crsr.style.top = dets.y + "px";
@@ -17,8 +20,12 @@ gsap.to("#nav" , {
     scrollTrigger : { 
         trigger :"#nav",
         scroller :"body",
-        start:"top -11%",
+        start:"top -10%",
         scrub:1,
+        // markers:true,
+        end:"top -11%",
+        
+
 
     },
 
@@ -32,6 +39,7 @@ gsap.to ("main", {
         start:"top -25%",
         end :"top -70%",
         scrub:2,
+      
 
     },
 
