d27d1eb32fa0297247a7652bb852f202d32d770f
Your Name <you@example.com>
2023-10-02 12:01:24 +0530

    jsbasic

---
+++ jscourse/hiteshsir/01_basic/variables.js
@@ -0,0 +1,24 @@
+/*
+now we talk about var let const 
+jscourse/hiteshsir/01_basic copy
+
+node jscourse/hiteshsir/01_basic/variables.js
+*/ 
+
+
+
+const accountId =144553 ;
+let accountEmail ="himanshuverma@gmail.com"
+var accountPassword ="123456";
+accountCity ="Jaipur"
+
+let accountState;
+
+
+accountEmail ="hv@GMAILL.com"
+accountPassword ="567890";
+accountCity ="ajmer"
+
+
+
+console.table([ accountId ,accountEmail,accountCity ,accountPassword ,accountState] );
