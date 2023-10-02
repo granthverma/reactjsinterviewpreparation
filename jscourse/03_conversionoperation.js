fe17551f064422100e25b3c0afb93e4fef633ddf
Your Name <you@example.com>
2023-10-02 14:07:07 +0530

    conversionoperation

---
+++ jscourse/03_conversionoperation.js
@@ -0,0 +1,32 @@
+// let score ="33jjjj";
+
+let score = null;
+//  console.log(typeof score);
+//  console.log(typeof(score));
+
+let valueInNumber =Number(score)
+// console.log(typeof valueInNumber);
+
+// console.log(valueInNumber);
+
+// "33" 👉🏻 33
+// "33abc "  👉🏻 NaN
+//  true  👉🏻 1  false  👉🏻 0
+//  null  👉🏻 0
+// undefined  👉🏻 Nan
+
+
+let isLoggedIn ="hitesh"
+
+let booleanIsLoggedIn = Boolean (isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// 1 => true ; 0  👉🏻 false
+// "" 👉🏻 false
+//"himanshu" 👉🏻 true 
+
+let someNumber =33;
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
