5aaa761327e7bd0b894c5aee07df0342e93a5f88
HIMANSHU VERMA <107205303+granthverma@users.noreply.github.com>
2023-11-23 10:49:30 +0530

    Update 04_comparision.js

--- jscourse/hiteshsir/01_basic/04_comparision.js
+++ jscourse/hiteshsir/01_basic/04_comparision.js
@@ -22,6 +22,14 @@ console.log(null == 0);
 console.log(null >=0);
 
 
+// true
+// true
+// false
+// false
+// false
+// true
+
+
 console.log(undefined >0);
 console.log(undefined == 0);
 console.log(undefined <0);
